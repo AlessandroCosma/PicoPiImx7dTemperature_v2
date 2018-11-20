11bb48b2e53d43dacae8217bc4bd527086a6cb69
Alessandro Cosma <alessandrocosma2@gmail.com>
2018-11-20 09:21:57 +0100

    Update ButtonLiveData
    Changed exit button from buttonC to buttonA

--- app/src/main/java/com/alessandrocosma/picopiimx7dtemperature/myviewmodel/mylivedata/ButtonLiveData.java
+++ app/src/main/java/com/alessandrocosma/picopiimx7dtemperature/myviewmodel/mylivedata/ButtonLiveData.java
@@ -1,12 +1,9 @@
 package com.alessandrocosma.picopiimx7dtemperature.myviewmodel.mylivedata;
 
-import com.alessandrocosma.picopiimx7dtemperature.MainActivity;
-import com.alessandrocosma.picopiimx7dtemperature.myviewmodel.MainActivityViewModel;
 import com.google.android.things.contrib.driver.button.Button;
 import com.google.android.things.contrib.driver.rainbowhat.RainbowHat;
 
 import android.arch.lifecycle.LiveData;
-import android.content.Context;
 import android.util.Log;
 
 import java.io.IOException;
@@ -16,7 +13,7 @@ public class ButtonLiveData extends LiveData<Button>{
 
      private static final String TAG = ButtonLiveData.class.getSimpleName();
 
-     private Button buttonC;
+     private Button butttonA;
 
 
      @Override
@@ -26,35 +23,36 @@ public class ButtonLiveData extends LiveData<Button>{
           Log.d(TAG, "onActive");
 
           try {
-               buttonC = RainbowHat.openButtonC();
-               buttonC.setOnButtonEventListener(new Button.OnButtonEventListener() {
+               butttonA = RainbowHat.openButtonA();
+               butttonA.setOnButtonEventListener(new Button.OnButtonEventListener() {
                     @Override
                     public void onButtonEvent(Button button, boolean pressed) {
-                         setValue(buttonC);
-                         Log.d(TAG, "button C pressed");
+                         setValue(butttonA);
+                         Log.d(TAG, "button A pressed");
                     }
                });
           }
           catch (IOException e){
-               Log.e(TAG, "Unable to open Button C");
+               Log.e(TAG, "Unable to open Button A");
           }
 
      }
 
      @Override
      protected void onInactive() {
-          super.onInactive();
-          Log.d(TAG, "onInactive");
 
-          try {
-               buttonC.close();
+        try {
+            buttonA.close();
 
-          }
-          catch (IOException e){
-               Log.e(TAG, "OnInactive: "+e);
-          }
+        }
+        catch (IOException e){
+            Log.e(TAG, "OnInactive: "+e);
+        }
 
-          setValue(null);
+        setValue(null);
+        
+        super.onInactive();
+        Log.d(TAG, "onInactive");
      }
 
 }
